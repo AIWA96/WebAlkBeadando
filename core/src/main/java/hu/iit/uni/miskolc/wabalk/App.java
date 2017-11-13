8a96695a179c868fe00fb8b5a5f6114513b66f68
Dani <szollosi.daniel0@gmail.com>
2017-11-13 08:26:19 +0100

    Init commit

---
+++ core/src/main/java/hu/iit/uni/miskolc/wabalk/App.java
@@ -0,0 +1,13 @@
+package hu.iit.uni.miskolc.wabalk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
